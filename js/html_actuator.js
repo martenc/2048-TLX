69606da3b29f9133a3f753c19314a5dd7f553859
Marten <martenc@gmail.com>
2014-07-20 15:00:04 -0700

    added product specs slider

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -131,7 +131,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Share your score! " + this.score;
+  var message = won ? "You win!" : "Sorry, game over. Try again!" + this.score;
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
